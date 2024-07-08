b580e78ac1d968618998a56db8b7e5dbd40c6953
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-07-08 11:35:38 +0200

    simplify default index page

--- www/index.php
+++ www/index.php
@@ -1,80 +1,63 @@
+<?php
+
+$DBuser = $_ENV['MYSQL_USER'];
+$DBpass = $_ENV['MYSQL_ROOT_PASSWORD'];
+$pdo = null;
+$db_version = null;
+
+try {
+    $database = 'mysql:host=database:3306';
+    $pdo = new PDO($database, $DBuser, $DBpass, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+
+    $db_version = $pdo->query('SELECT VERSION()')->fetchColumn();
+} catch (PDOException $e) {
+    // nothing
+}
+
+// Drop database connection
+$pdo = null;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
-        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
     </head>
 
     <body>
-        <section class="hero is-medium is-info is-bold">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK
-                    </h1>
-                    <h2 class="subtitle">
-                        Your local development environment
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <section class="section">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Environment</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><?= apache_get_version(); ?></li>
-                                <li>PHP <?= phpversion(); ?></li>
-                                <li>
-                                    <?php
-                                    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+        <h3>Welcome!</h3>
+        <ul>
+            <li>Apache version: <em><?= apache_get_version(); ?></em></li>
+            <li>PHP: <em><?= phpversion(); ?></em></li>
+            <li>
+                <?php
+                if ($db_version === null) {
+                    echo "Failed to connect to database";
+                } else {
+                    echo "MySQL Version: $db_version";
+                }
+                ?>
+            </li>
+        </ul>
 
-                                    /* check connection */
-                                    if (mysqli_connect_errno()) {
-                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
-                                    } else {
-                                        /* print server version */
-                                        printf("MySQL Server %s", mysqli_get_server_info($link));
-                                    }
-                                    /* close connection */
-                                    mysqli_close($link);
-                                    ?>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
-                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
-                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>
-                    <strong><a href="https://www.sprintcube.com" target="_blank">SprintCube</a></strong><br>
-                    The source code is released under the <a
-                        href="https://github.com/sprintcube/docker-compose-lamp/blob/master/LICENSE" target="_blank">MIT
-                        license</a>.
-                </p>
-            </div>
-        </footer>
+        <p>Quick links:</p>
+        <ul>
+            <li><a href="/phpinfo.php">phpinfo()</a></li>
+            <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
+        </ul>
+
+        <p>Secrets:</p>
+        <ul>
+            <li>MySQL User: <em><?= $DBuser ?></em></li>
+            <li>MySQL Password: <em><?= $DBpass ?></em></li>
+            <li>MySQL root password: <em><?= $_ENV['MYSQL_ROOT_PASSWORD'] ?></em></li>
+            <li>MySQL database name: <em><?= $_ENV['MYSQL_DATABASE'] ?></em></li>
+            <li>phpMyAdmin autologin password: <em><?= $_ENV['MYSQL_ROOT_PASSWORD'] ?></em></li>
+        </ul>
     </body>
 
 </html>
